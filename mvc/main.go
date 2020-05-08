a60b92655563c84edaa094ecb93696ed3121bcd8
Hieu <hieudoan2609@gmail.com>
2020-05-08 19:24:26 +0700

    update

--- mvc/main.go
+++ mvc/main.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "github.com/julienschmidt/httprouter"
     "fmt"
+    "github.com/hieudoan2609/go-webdev/mvc/models"
 )
 
 var users = map[string]models.User{}
@@ -12,35 +13,42 @@ var users = map[string]models.User{}
 func main() {
     r := httprouter.New()
     r.GET("/users/:id", getUser)
-    r.POST("/users", createUser)
+    r.POST("/users/:id", createUser)
     r.DELETE("/users/:id", deleteUser)
     http.ListenAndServe(":8080", r)
 }
 
 func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    u := models.User{
-        Name: "hieu",
-        Email: "hieudoan2609@gmail.com",
-    }
+    id := p.ByName("id")
+    u := users[id]
 
-    uj, err := json.Marshal(u)
-    check(err)
-
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOk)
-    fmt.Fprintf(w, "%s\n", uj)
+    uj, _ := json.Marshal(u)
+    respond(w, uj)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    
+    id := p.ByName("id")
+    u := models.User{}
+
+    json.NewDecoder(r.Body).Decode(&u)
+    users[id] = u
+
+    uj, _ := json.Marshal(users[id])
+    respond(w, uj)
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    
+    id := p.ByName("id")
+    u := users[id]
+
+    delete(users, id)
+
+    uj, _ := json.Marshal(u)
+    respond(w, uj)
 }
 
-func check(err error) {
-    if err != nil {
-        fmt.Println(err)
-    }
+func respond(w http.ResponseWriter, body []byte) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "%s\n", body)
 }
